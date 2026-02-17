15d23f694bfb150c41d1b829a60359e1955bb1ef
Ahmed El Gabri <ahmed@gabri.me>
2026-02-17 18:01:49 +0100

    chore: bump version to 0.7.0

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           default = self'.packages.git-wt;
           git-wt = pkgs.stdenvNoCC.mkDerivation {
             pname = "git-wt";
-            version = "0.6.0";
+            version = "0.7.0";
 
             src = ./.;
 
