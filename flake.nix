e01cb40007069725be947afb4dbcc73bb6127115
Ahmed El Gabri <ahmed@gabri.me>
2026-03-28 21:14:23 +0100

    chore: bump version to 2.0.0
    Bump the package version from 1.2.1 to 2.0.0 in flake.nix for the next
    major release.
    
    Agent prompts used in preparing this commit:
    
    > bump major version, commit and push
    
    Change-Id: I8a010ccc32475e4108616dfc433040c46a6a6964

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           default = self'.packages.git-wt;
           git-wt = pkgs.buildGoModule {
             pname = "git-wt";
-            version = "1.2.1";
+            version = "2.0.0";
 
             src = lib.cleanSource ./.;
 
