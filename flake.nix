57b2a1695594fbee6355d28e9b9c946a68aba36c
Ahmed El Gabri <ahmed@gabri.me>
2026-04-02 01:14:57 +0200

    chore: bump version to 2.0.1
    Change-Id: Iac1a4a15938305d182cfe5a5939e41916a6a6964

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           default = self'.packages.git-wt;
           git-wt = pkgs.buildGoModule {
             pname = "git-wt";
-            version = "2.0.0";
+            version = "2.0.1";
 
             src = lib.cleanSource ./.;
 
